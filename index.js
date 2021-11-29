69e8fb7656a8504921a5a21abc43937fa3396370
Gary Flugel <flugelg@gmail.com>
2021-11-29 12:25:10 -0500

    commit tree a023bc783c2f076ee1ccbc201349b40db320e5e1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(pickup){
+    return Math.abs(pickup - hq);
+}
+
+
+function distanceFromHqInFeet(pickup){
+    let stuff = distanceFromHqInBlocks(pickup);
+
+    return stuff * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination){
+    let feet;
+    if (start > destination){
+        feet = (start-destination)*264;
+        return feet;
+    }
+    else{
+        feet = (destination - start)*264;
+        return feet;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let travel = distanceTravelledInFeet(start, destination);
+
+    if (travel <=400 ) {
+        return 0;
+    }
+    else if (travel > 400 && travel < 2000) {  
+        return 2.56;   
+    }
+    else if (travel > 2000 && travel < 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
